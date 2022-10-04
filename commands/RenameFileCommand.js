ec19b716db1e535a8ccbfcb8c1874d558a796d05
Wasim Raja <wasim7raja10@gmail.com>
2022-10-05 00:34:07 +0530

    fixed bug in renaming file

--- commands/RenameFileCommand.js
+++ commands/RenameFileCommand.js
@@ -4,19 +4,20 @@ const fs = require("fs");
 class RenameFile extends Command {
 	constructor() {
 		super("rm", "renames 'file name' or 'file-name' to file_name", [
-			["<old separator>", "The folder which contains files to be renamed"],
+			["<path>", "The path of the folder which contains files"],
+			[("<old separator>", "The folder which contains files to be renamed")],
 			["<new separator>", "The folder which contains files to be renamed"],
 		]);
 	}
 
-	action(oldSep, newSep) {
+	action(path, oldSep, newSep) {
 		const temp = "/";
-		const files = fs.readdirSync(__dirname + temp);
+		const files = fs.readdirSync(path);
 		files.forEach((file) => {
 			const newName = file.split(oldSep).join(newSep);
-			fs.renameSync(__dirname + temp + file, __dirname + temp + newName);
+			fs.renameSync(path + temp + file, path + temp + newName);
 		});
-		this.result = "done.";
+		this.result = fs.readdirSync(path);
 	}
 }
 
