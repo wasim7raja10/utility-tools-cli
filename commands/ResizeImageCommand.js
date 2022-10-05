01c195644c51e5482fce7dedcddfce545925a1cc
Shubhadeep Das <dshubhadeep@gmail.com>
2022-10-05 23:27:16 +0530

    Added new arguments to resize image command

--- commands/ResizeImageCommand.js
+++ commands/ResizeImageCommand.js
@@ -1,21 +1,24 @@
 const sharp = require("sharp");
 const { Command } = require("./Command");
 const fs = require("fs");
+const { join } = require("path");
 
 class ResizeImage extends Command {
 	constructor() {
-		super("ri", "resize images", [["<path>"]]);
+		super("ri", "resize images", [["<path>"], ["<width>"], ["<height>"], ["<format>"]]);
 	}
 
-	action(pathInput) {
-		const temp = "/";
+	action(pathInput, width, height, format) {
 		const files = fs.readdirSync(pathInput);
 
 		files.forEach(async (file) => {
-			await sharp(pathInput + temp + file)
-				.webp({ quality: 80 }) // converting to webp format
-				.resize({ height: 427, width: 640 }) // resize dimension of image
-				.toFile(file.split(".")[0] + ".webp"); // changing the extension of file to .webp
+			try {
+				await sharp(join(pathInput, file))
+					.resize({ height: Number(height), width: Number(width) }) // resize dimension of image
+					.toFile(`${file.split(".")[0]}.${format}`); // changing the extension of file to given format
+			} catch (err) {
+				console.error(`Failed to resize ${file} :: ${err.message}`);
+			}
 		});
 		this.result = fs.readdirSync(pathInput);
 	}
