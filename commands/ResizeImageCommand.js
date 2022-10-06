36b9115708e0b3afa5a0391473709fa0489a2fb2
Saurabh Kumar~ <hmsaurabhkr@gmail.com>
2022-10-06 18:49:38 +0530

    resolve conflict

--- commands/ResizeImageCommand.js
+++ commands/ResizeImageCommand.js
@@ -4,24 +4,6 @@ const fs = require("fs");
 const { join } = require("path");
 
 class ResizeImage extends Command {
-<<<<<<< HEAD
-  constructor() {
-    super("ri", "resize images", [["<path>"]]);
-  }
-
-  action(pathInput) {
-    const temp = "/";
-    const files = fs.readdirSync(pathInput);
-
-    files.forEach(async file => {
-      await sharp(pathInput + temp + file)
-        .webp({ quality: 80 }) // converting to webp format
-        .resize({ height: 427, width: 640 }) // resize dimension of image
-        .toFile(file.split(".")[0] + ".webp"); // changing the extension of file to .webp
-    });
-    this.result = fs.readdirSync(pathInput);
-  }
-=======
 	constructor() {
 		super("ri", "resize images", [["<path>"], ["<width>"], ["<height>"], ["<format>"]]);
 	}
@@ -43,7 +25,6 @@ class ResizeImage extends Command {
 		});
 		this.result = fs.readdirSync(pathInput);
 	}
->>>>>>> 34a1f339d8b8279c60f151f7db11f5caaa33376f
 }
 
 module.exports = ResizeImage;
