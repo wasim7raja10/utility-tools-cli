df1c44387dc53d7e5ca4023f4a16ea84f35c36b8
Saurabh Kumar~ <hmsaurabhkr@gmail.com>
2022-10-06 18:43:38 +0530

    formating

--- commands/AddTextCommand.js
+++ commands/AddTextCommand.js
@@ -3,15 +3,15 @@ const { Command } = require("./Command");
 const fs = require("fs");
 
 class AddText extends Command {
-	constructor() {
-		super("rt", "add short text on images", [["<path>"], ["<text>"]]);
-	}
+  constructor() {
+    super("rt", "add short text on images", [["<path>"], ["<text>"]]);
+  }
 
-	action(pathInput, text) {
-		const temp = "/";
-		const files = fs.readdirSync(pathInput);
-        // const text = "Watermark Image ";
-        const svgImage = `
+  action(pathInput, text) {
+    const temp = "/";
+    const files = fs.readdirSync(pathInput);
+    // const text = "Watermark Image ";
+    const svgImage = `
         <svg class="vg">
         <style>
         .vg{ height: 100px; width: 100%;}
@@ -20,21 +20,20 @@ class AddText extends Command {
         <text x="50%" y="50%" text-anchor="middle" class="title">${text}</text>
         </svg>
         `;
-        const svgBuffer = Buffer.from(svgImage);
+    const svgBuffer = Buffer.from(svgImage);
 
-		files.forEach(async (file) => {
-			await sharp(pathInput + temp + file )
-                .composite([
-                    {
-                      input: svgBuffer,
-                      gravity: sharp.gravity.southeast,
-
-                    },
-                  ])
-                  .toFile( file.split(".")[0] + "_watermark" + ".webp");
-		});
-		this.result = fs.readdirSync(pathInput);
-	}
+    files.forEach(async file => {
+      await sharp(pathInput + temp + file)
+        .composite([
+          {
+            input: svgBuffer,
+            gravity: sharp.gravity.southeast,
+          },
+        ])
+        .toFile(file.split(".")[0] + "_watermark" + ".webp");
+    });
+    this.result = fs.readdirSync(pathInput);
+  }
 }
 
 module.exports = AddText;
--- commands/ResizeImageCommand.js
+++ commands/ResizeImageCommand.js
@@ -3,22 +3,22 @@ const { Command } = require("./Command");
 const fs = require("fs");
 
 class ResizeImage extends Command {
-	constructor() {
-		super("ri", "resize images", [["<path>"]]);
-	}
+  constructor() {
+    super("ri", "resize images", [["<path>"]]);
+  }
 
-	action(pathInput) {
-		const temp = "/";
-		const files = fs.readdirSync(pathInput);
+  action(pathInput) {
+    const temp = "/";
+    const files = fs.readdirSync(pathInput);
 
-		files.forEach(async (file) => {
-			await sharp(pathInput + temp + file)
-				.webp({ quality: 80 }) // converting to webp format
-				.resize({ height: 427, width: 640 }) // resize dimension of image
-				.toFile(file.split(".")[0] + ".webp"); // changing the extension of file to .webp
-		});
-		this.result = fs.readdirSync(pathInput);
-	}
+    files.forEach(async file => {
+      await sharp(pathInput + temp + file)
+        .webp({ quality: 80 }) // converting to webp format
+        .resize({ height: 427, width: 640 }) // resize dimension of image
+        .toFile(file.split(".")[0] + ".webp"); // changing the extension of file to .webp
+    });
+    this.result = fs.readdirSync(pathInput);
+  }
 }
 
 module.exports = ResizeImage;
